7eafe7e9039f1a1f2aafc64974681993992278e3
abhigitit <90536339+abhigitit@users.noreply.github.com>
2022-04-13 13:20:00 -0700

    Admin Page

---
+++ piedpiper-frontend/src/Pages/Employee.js
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+import React from "react";
+import { useAuth0, withAuthenticationRequired } from "@auth0/auth0-react";
+
+const AdminDashboard = () => {
+  const { user } = useAuth0();
+
+  return (
+    <div className="text-center">
+      <h1 className="main-para">Admin Dashboard</h1>
+
+      <Link to="/UserData">
+        <button className="primary-button">See User Data</button>
+      </Link>
+
+      <Link to="/AVData">
+        <button className="primary-button">See AV Data</button>
+      </Link>
+    </div>
+  );
+};
+export default AdminDashboard;
