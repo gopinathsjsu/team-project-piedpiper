22fa2b96d60cb4ccbd218db0a59ee26c7b6571f4
santhoshsai3 <47252929+santhoshsai3@users.noreply.github.com>
2022-05-04 14:01:36 -0700

    added customer and hotel endpoints

---
+++ piedpiper-backend/src/main/java/com/piedPiper/controller/CustController.java
@@ -0,0 +1,124 @@
+package com.piedpiper.controller;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.piedpiper.api.BookingResponse;
+import com.piedpiper.exception.GenericException;
+import com.piedpiper.model.Reservation;
+import com.piedpiper.model.Customer;
+import com.piedpiper.repository.BookingRepository;
+import com.piedpiper.repository.CustRepo;
+import com.piedpiper.repository.HotelRepository;
+import com.piedpiper.service.CustServiceImpl;
+import com.piedpiper.service.HotelServiceImpl;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping("/hotel-booking")
+public class CustController {
+
+	@Autowired
+	public HotelRepository hotelrepository;
+
+	@Autowired
+	public BookingRepository bookingRepository;
+
+	@Autowired
+	public CustRepo custRepo;
+
+	public HotelServiceImpl hotelservice;
+
+	@Autowired
+	private CustServiceImpl customerService;
+
+	@Autowired
+	public CustController(HotelServiceImpl hotelservice) {
+		this.hotelservice = hotelservice;
+	}
+
+	@PostMapping("/registerCustomer")
+	public String registerCustomer(@RequestBody Customer customer) {
+
+		return "registered";
+	}
+
+	@PostMapping("/createBooking")
+	public ResponseEntity<BookingResponse> createBooking(@Valid @RequestBody Reservation customer) {
+
+		BookingResponse bookingResponseDetails = hotelservice.createBooking(customer);
+
+		return new ResponseEntity<BookingResponse>(bookingResponseDetails, HttpStatus.CREATED);
+	}
+
+	@PutMapping("/updateBooking/{reservationID}")
+	public ResponseEntity<Reservation> updateBooking(@Valid @PathVariable("reservationID") String reservationID,
+			@Valid @RequestBody Reservation reservation) {
+
+		ResponseEntity<Reservation> updatedDetails = hotelservice.updateBookingDetails(reservationID,
+				reservation);
+
+		return new ResponseEntity<Reservation>(updatedDetails.getBody(), HttpStatus.OK);
+
+	}
+
+	@CrossOrigin(origins = "http://localhost:3000")
+	@GetMapping("/getBooking/{emailID}")
+	public List<Reservation> getBookingOfCustomer(@Valid @PathVariable("emailID") String emailID) {
+
+		List<Reservation> reservation = bookingRepository.findByEmail(emailID);
+
+		return reservation;
+	}
+
+	@GetMapping("/getRewardPoints/{emailID}")
+	public String getRewardPoints(@Valid @PathVariable("emailID") String emailID) {
+
+		Customer customerexists = customerService.findUserByEmail(emailID);
+		double sum = 0;
+
+		if (null != customerexists) {
+
+			List<Reservation> reservation = bookingRepository.findByEmail(emailID);
+
+			if (!reservation.isEmpty() && reservation != null) {
+
+				sum = reservation.get(reservation.size() - 1).getRewardpoints();
+				// bookingDetails.stream().mapToDouble(de -> de.getRewardpoints()).sum();
+
+				return " your reward points are " + sum;
+
+			} else {
+
+				throw new GenericException("No customer with " + emailID + " exists");
+			}
+
+		} else {
+
+			throw new GenericException("No customer with " + emailID + " exists");
+		}
+
+	}
+
+	@DeleteMapping("/cancel/{reservationID}")
+	public String cancelBooking(@PathVariable("reservationID") String reservationID) {
+
+		hotelservice.deleteBookingDetails(reservationID);
+		return "booking cancelled";
+	}
+
+}
---
+++ piedpiper-backend/src/main/java/com/piedPiper/controller/HotelController.java
@@ -0,0 +1,107 @@
+package com.piedpiper.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.piedpiper.api.EmployeeAuthenticationRequest;
+import com.piedpiper.exception.GenericException;
+import com.piedpiper.model.Reservation;
+import com.piedpiper.model.Employee;
+import com.piedpiper.model.Hotel;
+import com.piedpiper.repository.EmployeeRepository;
+import com.piedpiper.repository.HotelRepository;
+import com.piedpiper.service.HotelServiceImpl;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:3000", maxAge = 40000)
+public class HotelController {
+
+	@Autowired
+	MongoTemplate mongoTemplate;
+
+	@Autowired
+	public HotelRepository hotelrepository;
+
+	@Autowired
+	public EmployeeRepository employeeRepository;
+
+	public HotelServiceImpl hotelservice;
+
+	@Autowired
+	public HotelController(HotelServiceImpl hotelservice) {
+		this.hotelservice = hotelservice;
+	}
+
+	@GetMapping("/searchHotel/{location}")
+	@CrossOrigin(origins = "http://localhost:3000")
+	public List<Hotel> searchHotel(@PathVariable String location) {
+
+		return hotelservice.searchHotelByLocation(location);
+
+	}
+
+	@GetMapping("/getBooking")
+	@CrossOrigin(origins = "http://localhost:3000")
+	public List<Reservation> getBooking() {
+
+		return hotelservice.getAllBookingDetails();
+	}
+
+	@PostMapping("/savehotel")
+	@CrossOrigin(origins = "http://localhost:3000")
+	public String saveHotel(@Valid @RequestBody Hotel hotel) {
+		hotelrepository.save(hotel);
+
+		return "added Hotel";
+
+	}
+
+	@GetMapping("/hotel/{id}")
+	@CrossOrigin(origins = "http://localhost:3000")
+	public Optional<Hotel> getHotel(@PathVariable int id) {
+		return hotelrepository.findById(id);
+
+	}
+
+	@PostMapping("/employee/register")
+	@CrossOrigin(origins = "http://localhost:3000")
+	public String registerEmployee(@Valid @RequestBody Employee employee) {
+
+		Employee employeeExists = employeeRepository.findUserByEmail(employee.getEmail());
+		if (employeeExists != null) {
+			throw new GenericException("Employee with email: " + employee.getEmail() + " already exists. Please Login");
+		} else {
+			employeeRepository.save(employee);
+
+			return "employee registered successfully";
+		}
+	}
+
+	@PostMapping("/employee/login")
+	@CrossOrigin(origins = "http://localhost:3000")
+	public String employeeLogin(@Valid @RequestBody EmployeeAuthenticationRequest employee) {
+
+		Employee employeeExists = employeeRepository.findUserByEmail(employee.getEmail());
+		if (employeeExists != null) {
+			return "signin succesfull";
+		} else {
+			throw new GenericException(
+					"Employee with email: " + employee.getEmail() + " doesn't exists. Please SignUp");
+		}
+
+	}
+
+}
