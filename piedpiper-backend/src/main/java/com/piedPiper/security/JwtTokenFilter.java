f9ab7431d370a50a13646bfd2a77a08669668a97
santhoshsai3 <47252929+santhoshsai3@users.noreply.github.com>
2022-05-14 14:12:16 -0700

    integrated JWT

---
+++ piedpiper-backend/src/main/java/com/piedPiper/security/JwtTokenFilter.java
@@ -0,0 +1,33 @@
+package com.piedpiper.security;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.GenericFilterBean;
+
+public class JwtTokenFilter extends GenericFilterBean {
+
+	private JwtTokenGenerator jwtTokenGenerator;
+	
+    public JwtTokenFilter(JwtTokenGenerator jwtTokenProvider) {
+        this.jwtTokenGenerator = jwtTokenProvider;
+    }
+    
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain filterChain)
+        throws IOException, ServletException {
+        String token = jwtTokenGenerator.resolveToken((HttpServletRequest) req);
+        if (token != null && jwtTokenGenerator.validateToken(token)) {
+            Authentication auth = token != null ? jwtTokenGenerator.getAuthentication(token) : null;
+            SecurityContextHolder.getContext().setAuthentication(auth);
+        }
+        filterChain.doFilter(req, res);
+    }
+}
